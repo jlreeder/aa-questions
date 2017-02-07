789d7f3459ae16050efdb3b5e19d73b6b8793022
Jason Reeder <info@jasonreeder.com>
2017-02-07 14:44:05 -0800

    Add method

--- replies.rb
+++ replies.rb
@@ -31,6 +31,20 @@ class Reply
     replies.map { |ele| Reply.new(ele) }
   end
 
+  def self.find_by_question_id(question_id)
+    replies = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        question_id = ?
+      SQL
+
+    return nil if replies.empty?
+    replies.map { |ele| Reply.new(ele) }
+  end
+
   def initialize(options)
     @id = options['id']
     @body = options['body']
@@ -43,4 +57,5 @@ end
 
 
 # reply = Reply.find_by_id(1)
-p Reply.find_by_user_id(2)
+# p Reply.find_by_user_id(2)
+p Reply.find_by_question_id(1)

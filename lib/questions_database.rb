1d2ed73751dd3fb9a8c017806f7ae8ce5d571134
Jason Reeder <info@jasonreeder.com>
2017-02-07 17:56:35 -0800

    Correct file path

--- lib/questions_database.rb
+++ lib/questions_database.rb
@@ -5,7 +5,7 @@ class QuestionsDatabase < SQLite3::Database
   include Singleton
 
   def initialize
-    super('../student_questions.db')
+    super('student_questions.db')
     self.type_translation = true
     self.results_as_hash = true
   end
